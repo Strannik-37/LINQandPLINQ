cd931810664b22427ccf32ffac1e6e1e92ce20ce
Alexandr Lezin <lezin.sasha-37@mail.ru>
2021-05-14 22:56:25 +0300

    Работа с LINQ и PLINQ

---
+++ LinqAndPlinq/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LinqAndPlinq
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Massiv massiv = new Massiv();
+        private void btGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int N = Convert.ToInt32(tbN.Text);
+            massiv.Generate(N);
+            tbGenerate.Text = "";
+           foreach (string element in massiv.mass)
+           {
+               tbGenerate.Text = tbGenerate.Text + element + '\n';
+           }
+        }
+
+        private void btStart_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            tbRezult.Text = "";
+            sw.Restart();
+            string[] result = GetRezult();
+            sw.Stop();
+            foreach (string element in result)
+            {
+                tbRezult.Text = tbRezult.Text + element + '\n';
+            }
+            tbTime.Text = Convert.ToString(sw.Elapsed);
+        }
+
+        private string[] GetRezult()
+        {
+            switch (cmbAction.SelectedIndex)
+            {
+                case 0:
+                    return massiv.EvenNumbers();
+                case 1:
+                    return massiv.EvenNumbers_Parallel();
+                case 2:
+                    return massiv.OddNumbers();
+                case 3:
+                    return massiv.OddNumbers_Parallel();
+                case 4:
+                    return massiv.SummUpperSix();
+                case 5:
+                    return massiv.SummUpperSix_Parallel();
+                case 6:
+                    return massiv.SumDigitsThirteen();
+                case 7:
+                    return massiv.SumDigitsThirteen_Parallel();
+                default:
+                    return massiv.EvenNumbers();
+            }
+        }
+    }
+}
---
+++ LinqAndPlinq/Massiv.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndPlinq
+{
+    class Massiv
+    {
+        public string[] mass;
+        public void Generate(int size)
+        {
+            Random rnd = new Random();
+            mass = new string[size];
+            for(int i = 0; i < size; i++)
+            {
+                mass[i] = rnd.Next().ToString();
+            }
+        }
+        public string [] EvenNumbers()
+        {
+            string[] rez;
+            rez = mass.Where(s => Convert.ToInt32(s.Last()) % 2 == 0).OrderByDescending(s=>Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] EvenNumbers_Parallel()
+        {
+            string[] rez;
+            rez = mass.AsParallel().Where(s => Convert.ToInt32(s.Last()) % 2 == 0).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] OddNumbers()
+        {
+            string[] rez;
+            rez = mass.Where(s => Convert.ToInt32(s.Last()) % 2 == 1).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] OddNumbers_Parallel()
+        {
+            string[] rez;
+            rez = mass.AsParallel().Where(s => Convert.ToInt32(s.Last()) % 2 == 1).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] SummUpperSix()
+        {
+            string[] rez;
+            rez = mass.Where(s => s.Length > 3 && Convert.ToInt32(Convert.ToString(s[1])) + Convert.ToInt32(Convert.ToString(s[s.Length - 2])) == 6).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] SummUpperSix_Parallel()
+        {
+            string[] rez;
+            rez = mass.AsParallel().Where(s => s.Length > 3 && Convert.ToInt32(Convert.ToString(s[1])) + Convert.ToInt32(Convert.ToString(s[s.Length - 2])) == 6).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] SumDigitsThirteen()
+        {
+            string[] rez;
+            rez = mass.Where(s => s.Sum(x => Convert.ToInt32(Convert.ToString(x))) == 13).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+        public string[] SumDigitsThirteen_Parallel()
+        {
+            string[] rez;
+            rez = mass.AsParallel().Where(s => s.Sum(x => Convert.ToInt32(Convert.ToString(x))) == 13).OrderByDescending(s => Convert.ToInt32(s)).ToArray();
+            return rez;
+        }
+    }
+}
